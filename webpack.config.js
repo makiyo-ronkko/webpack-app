059437cc835e9fb337e9abd6ee7d759155757eef
makiyo <makiyo@makiyos-MacBook-Pro.local>
2021-03-22 22:04:17 +0200

    Plugin sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,14 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'], // reverse order
+				// test: /\.css$/,
+				// use: ['style-loader', 'css-loader'], // reverse order
+				test: /\.scss$/,
+				use: [
+					'style-loader', // 3. Inject styles into DOM
+					'css-loader', // 2. Turns css into commonjs
+					'sass-loader', // 1. Turns sass into css
+				],
 			},
 		],
 	},
